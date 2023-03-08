f8de6b902a6642b9385ac88265d22acd1fd3a637
ertaevaaiym1 <86400628+ertaevaaiym1@users.noreply.github.com>
2023-03-08 18:01:21 +0600

    Add files via upload

---
+++ lab6/albums.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Album} from "./models";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumsService {
+  BASE_URL = "https://jsonplaceholder.typicode.com"
+  constructor(private client: HttpClient) { }
+
+  getAlbums() : Observable<Album[]> {
+    return this.client.get<Album[]>(`${this.BASE_URL}/albums`)
+  }
+
+  getAlbum(id: number) : Observable<Album>{
+    return this.client.get<Album>(`${this.BASE_URL}/albums/${id}`)
+  }
+
+  // addAlbum(album: Album) : Observable<Album>{
+  //   return this.client.get<Album>(`${this.BASE_URL}/albums`, album)
+  // }
+}
